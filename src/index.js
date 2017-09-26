63c9b700e5b9eccb668e687a7ff53eb317b5e8ae
anton <prozor.anton@gmail.com>
2017-09-26 15:42:05 +0300

    first lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+var array = new Array();
+for (var i = 0; i < str.length; i++) {
+    array[i] = str[i];   
+}
+
+if (array.length % 2 == 1) {
+    //console.log('false');
+    return false;
+}
+else {
+    for (var i = 0; i < array.length; ++i) {
+        //console.log('i = ', i);
+        var flag = false;
+        for (var j = 0; j < bracketsConfig.length; ++j) {
+            //console.log('j = ', j);
+            if(array[i] == bracketsConfig[j][0] && array[i + 1] == bracketsConfig[j][1] ) {
+                //console.log(array[i] == bracketsConfig[j][k] && array[i + 1] == bracketsConfig[j][k + 1]);
+                array.splice(i, 2);
+                //console.log(array);
+                i = -1;
+                break;
+            }
+        }       
+    }
+}
+
+if(array.length == 0){
+    return true;
+}
+else{
+    return false;
+}
 }
